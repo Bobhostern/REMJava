01254901134247226809cc53f15a8aeb73fd96b8
Bob Hosten <bob.hostern@gmail.com>
2013-05-03 13:21:37 -0500

    Update: Added Version module

---
+++ v0.2-alpha-0-src/src/infoClass/VersionEnum.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package infoClass;
+
+/**
+ * These are the enums for versioning and what they represent.
+ * @author onigiri
+ */
+public enum VersionEnum {
+    /**
+     * ALPHA - barely different from the previous release. 
+     * Use the other release unless you need something that was introduced in this release.
+     */
+    ALPHA,
+    /**
+     * BETA - quite different, but not there yet, or this current release is buggy.
+     * Use at your own discretion.
+     */
+    BETA,
+    /**
+     * GAMMA - the final release for a version.
+     * Use this if in doubt.
+     */
+    GAMMA
+}
---
+++ v0.2-alpha-0-src/src/infoClass/version/Version.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package infoClass.version;
+
+import infoClass.VersionEnum;
+
+/**
+ *
+ * @author onigiri
+ */
+public class Version {
+    public static final double versionMajorNumber = 0.2;
+    public static final VersionEnum versionStage = VersionEnum.ALPHA;
+    public static final int versionStageNumber = 0;
+    public static final String versionString = "0.2:ALPHA:0";
+}
